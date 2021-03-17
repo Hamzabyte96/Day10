b725d32fc49cfd7c2cf73b19f7ee4078a8f273ef
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-17 20:23:30 +0100

    izmjene ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+var myPet = {
+    species: "Pet",
+    name: "Rex",
+    legs: 4,
+    friends : "Tom, Jerry"
+}
+
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
