3520b9beac4ddd42ffa5f3d4aae13187838fe3ee
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-17 23:00:30 +0100

    izmjene ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,14 @@
+var lion = {
+    "name": "Simba",
+    "legs": 4,
+    "tails": 1,
+    "roar": "roar-roar"
+};
+
+function myFunction(roar) {
+    delete lion[roar];
+    return lion;
+}
+myFunction("roar");
+console.log(myFunction("roar"));
+module.exports = myFunction;
